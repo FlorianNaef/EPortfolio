cb16be8943a3a956e2f5c66826aa78aa14a22f1e
FlorianNaef <florian.naef@protonmail.com>
2022-12-14 10:41:56 +0100

    front page slider added

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var options = {
+        fullWidth: true,
+        indicators: true
+    };
+    var instances = M.Carousel.init(elems, options);
+
+    let indicatorItems = document.querySelectorAll('.carousel .indicator-item');
+
+    setInterval(() => {
+        indicatorItems.forEach(el => {
+        if (el.classList.contains("active")) {
+            sib = el.nextElementSibling;
+            if (sib == null) {
+            indicatorItems[0].click();
+            } else {
+            sib.click()
+            }
+        }
+        });
+    }, 6000);
+});
